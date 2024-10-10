4b6e18744c585dd1b87c6704f3010dd8d2e82952
Jasdeep Singh <jasdeepsingh9900@gmail.com>
2024-10-11 01:00:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+// import { useState } from "react"
+// import Navbar from "./Components/Navbar"
+// import NewsBoard from "./Components/NewsBoard"
+
+// const App = () => {
+//   const [category,setCategory] = useState("general");
+//   return (
+//     <div>
+//       <Navbar setCategory={setCategory}/>
+//       <NewsBoard category={category}/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+import { useState } from "react";
+import Navbar from "./Components/Navbar";
+import NewsBoard from "./Components/NewsBoard";
+
+const App = () => {
+  const [category, setCategory] = useState("general");
+  
+  return (
+    <div>
+      <Navbar setCategory={setCategory} currentCategory={category} />
+      <NewsBoard category={category} />
+    </div>
+  );
+};
+
+export default App;
